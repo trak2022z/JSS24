4f35844c411984aefd0919f9cc50c65ff0065d22
Tom Krok <91782249+trak2022z@users.noreply.github.com>
2022-03-23 22:44:57 +0000

    Solution

--- script.js
+++ script.js
@@ -18,9 +18,51 @@
    * TODO
    * adds a blog entry to the blog post page
    */
-  function addEntry() {
+  
+  //
+function addEntry() {
+    let date = id("date").value;
+    let entry = id("entry").value;
+    let container = document.createElement("article");
+    container.classList.add("post");
+    let header = document.createElement("h3");
+    let contents = document.createElement("p");
+    header.textContent = "Date: " + date;
+    contents.textContent = "Entry: " + entry;
+    container.appendChild(header);
+    container.appendChild(contents);
+    id("posts").appendChild(container);
+    container.addEventListener("dblclick", removePost);
+    id("date").value = "";
+    id("entry").value = "";
+    checkNumPosts();
   }
 
+  function checkNumPosts() {
+    let posts = qsa(".post");
+    let numPosts = posts.length;
+    let addEntryBtn = qs("button");
+    if (numPosts === 3) {
+      addEntryBtn.disabled = true;
+    } else {
+      addEntryBtn.disabled = false;
+    }
+  }
+
+  /**
+   * removes the selected blog post
+   */
+  function removePost() {
+    this.parentNode.removeChild(this);
+    checkNumPosts();
+  }
+
+
+
+
+
+
+  
   /**
    * Returns the element that has the ID attribute with the specified value.
    * @param {string} name - element ID.
@@ -38,4 +80,23 @@
   function qs(selector) {
     return document.querySelector(selector);
   }
+
+
+
+  
+
+  //
+  /**
+   * Returns the array of elements that match the given CSS selector.
+   * @param {string} query - CSS query selector
+   * @returns {object[]} array of DOM objects matching the query.
+   */
+   function qsa(query) {
+    return document.querySelectorAll(query);
+  }
+
+
+
+
+  
 })();
